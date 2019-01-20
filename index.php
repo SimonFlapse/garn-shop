c76ac172b29734c86e23ddea1113ab49c001c25c
luckasv1 <luckasv1@Hotmail.com>
2019-01-20 23:19:42 +0100

    Update

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
     </head>
 <body>
     <?php require_once 'process.php'?>
@@ -17,9 +18,9 @@
 		$_SESSION['language_id'] = $default_language_id;
 	} elseif (isset($_GET['language'])){
 		$_SESSION['language_id'] = $_GET['language'];
-	}		
+	}
 	
-    if (isset($_SESSION['message'])):		?>
+    if (isset($_SESSION['message'])):?>
 
     <div class="alert alert-<?=$_SESSION['msg_type']?>">
 
@@ -51,60 +52,139 @@
                     </tr>
                 </thead>
                 <?php
-                    while ($row = $result->fetch_assoc()): ?>
+                    while ($row = $result->fetch_assoc()):
+                        
+                        $result_default = $mysqli->query("SELECT * FROM products INNER JOIN products_description ON products.products_id = products_description.products_id WHERE products_description.languages_id = $default_language_id AND products.products_id = ".$row['products_id']."") or die ($mysqli->error);
+                        $row_default = $result_default->fetch_assoc()
+                    ?>
                         <tr>
                             <td> <?php echo $row['products_description_name']?> </td>
                             <td> <?php echo $row['products_price']?> </td>
                             <td> <?php echo $row['products_reference']?> </td>
                             <td>
-                                <a href="index.php?edit=<?php echo $row['products_id']; ?>"
-                                class="btn btn-info">Ændre</a>
 
-                                <a href="index.php?delete=<?php echo $row['products_id']; ?>"
-                                class="btn btn-danger">Slet</a>
+                                <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal<?php echo $row['products_id']?>">Rediger</button>
+                                <div id="myModal<?php echo $row['products_id']?>" class="modal fade" role="dialog">
+                                <div class="modal-dialog modal-lg">
+
+                                    <!-- Modal content-->
+                                    <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">Redigere vare</h4>
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    </div>
+                                    <div class="modal-body">
+                                    <div class="row justify-content-center">
+                                        <form action="process.php" method="POST">
+                                                <div class="form-group">
+                                                    <label>Varenummer</label>
+                                                    <input type="text" name="product" value="<?php echo $row['products_reference']?>">
+                                                </div> 
+                                                <div class="form-group">
+                                                    <label>Pris</label>
+                                                    <input type="text" name="price" value="<?php echo $row['products_price']?>">
+                                                </div>
+                                                <div class="container">
+                                                    <div class="row">
+                                                        <div class="col-sm">
+                                                            <div class="form-group">
+                                                                <label>Varenavn</label>
+                                                                <input type="hidden" name="id" value="<?php echo $row['products_id']?>">
+                                                                <input type="text" name="name" value="<?php echo $row['products_description_name']?>">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label>Korttekst</label> <br>
+                                                                <textarea rows="4" cols="40" style="resize: none" type="text" name="short"><?php echo $row['products_description_short_description']?></textarea>
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label>Langtekst</label> <br>
+                                                                <textarea rows="8" cols="40" style="resize: none" type="text" name="long"><?php echo $row['products_description_description']?></textarea>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-sm">
+                                                            <div class="form-group">
+                                                                <label>Varenavn</label>
+                                                                <input type="text" value="<?php echo $row_default['products_description_name']?>" disabled>
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label>Korttekst</label> <br>
+                                                                <textarea rows="4" cols="40" style="resize: none" type="text" disabled><?php echo $row_default['products_description_short_description']?></textarea>
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label>Langtekst</label> <br>
+                                                                <textarea rows="8" cols="40" style="resize: none" type="text" disabled><?php echo $row_default['products_description_description']?></textarea>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-primary" name="edit">Gem</button>
+                                                </div>
+                                            </form>
+                                            </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                                    </div>
+
+                                </div>
+                                </div>
+
+                                <a href="index.php?delete=<?php echo $row['products_id']; ?>" class="btn btn-danger">Slet</a>
 
                             </td>
                         </tr>
                     <?php endwhile; ?>
             </table>
         </div>
-        <?php
-        // pre_r($result->fetch_assoc());
 
-        function pre_r( $array ) {
-            echo '<pre>';
-            print_r($array);
-            echo '</pre>';
-        }
-    ?>
+        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#nyVare">Ny vare</button>
+        <div id="nyVare" class="modal fade" role="dialog">
+            <div class="modal-dialog modal-lg">
 
-    <div class="row justify-content-center">
-    <form action="process.php" method="POST">
-		<div class="form-group">
-            <label>Varenavn</label>
-            <input type="text" name="name" placeholder="Tilføj varenavn">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Ny vare</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row justify-content-center">
+                            <form action="process.php" method="POST">
+                            <div class="form-group">
+                                <label>Varenummer</label>
+                                <input type="text" name="product" placeholder="Tilføj vare">
+                            </div>
+                            <div class="form-group">
+                                <label>Pris</label>
+                                <input type="text" name="price" placeholder="Tilføj pris">
+                            </div>
+                            <div class="form-group">
+                                <label>Varenavn</label>
+                                <input type="text" name="name" placeholder="Tilføj varenavn">
+                            </div>
+                            <div class="form-group">
+                                <label>Korttekst</label> <br>
+                                <textarea rows="4" cols="50" style="resize: none" type="text" name="short" placeholder="Tilføj korttekst"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>Langtekst</label> <br>
+                                <textarea rows="8" cols="50" style="resize: none" type="text" name="long" placeholder="Tilføj langtekst"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary" name="save">Gem</button>
+                            </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="form-group">
-            <label>Varenummer</label>
-            <input type="text" name="product" placeholder="Tilføj vare">
-        </div>
-        <div class="form-group">
-            <label>Pris</label>
-            <input type="text" name="price" placeholder="Tilføj pris">
-        </div>
-        <div class="form-group">
-            <label>Korttekst</label>
-            <input type="text" name="short" placeholder="Tilføj korttekst">
-        </div>
-        <div class="form-group">
-            <label>Langtekst</label>
-            <input type="text" name="long" placeholder="Tilføj langtekst">
-        </div>
-        <div class="form-group">
-            <button type="submit" class="btn btn-primary" name="save">Gem</button>
-        </div>
-    </form>
-    </div>
+
 	<div class="footer fixed-bottom">
 	<div class="row justify-content-center">
 	<?php
--- process.php
+++ process.php
@@ -3,6 +3,7 @@
 session_start();
 
 $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
+mysqli_query($mysqli, "SET NAMES 'utf8'");
 
 if (isset($_POST['save'])){
     $name = $_POST['name'];
@@ -25,10 +26,31 @@ if (isset($_POST['save'])){
 
 if (isset($_GET['delete'])){
     $id = $_GET['delete'];
+
     $mysqli->query("DELETE products, products_description FROM products INNER JOIN products_description ON products.products_id = products_description.products_id  WHERE products.products_id=$id") or die($mysqli->error);
 
     $_SESSION['message'] = "Slettet!";
     $_SESSION['msg_type'] = "danger";
 
     header("location: index.php");
+}
+
+if (isset($_POST['edit'])){
+    $name = $_POST['name'];
+    $product = $_POST['product'];
+    $price = $_POST['price'];
+	$short = $_POST['short'];
+    $long = $_POST['long'];
+    $products_id = $_POST['id'];
+
+    $mysqli->query("UPDATE products SET products_reference = '$product', products_price = '$price' WHERE products_id = $products_id") or
+        die($mysqli->error);
+		$last_id = $mysqli->insert_id;
+	$mysqli->query("UPDATE products_description SET products_description_name = '$name', products_description_short_description = '$short', products_description_description = '$long' WHERE products_id = $products_id AND languages_id = ".$_SESSION['language_id']."") or
+        die($mysqli->error);
+		
+        $_SESSION['message'] = "Gemt!";
+        $_SESSION['msg_type'] = "success";
+
+        header("location: index.php");
 }
